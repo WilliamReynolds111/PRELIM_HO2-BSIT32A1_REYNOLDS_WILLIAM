304fe06290f142c243cd1d0769a93c4c885eadd4
WilliamReynolds111 <162089947+WilliamReynolds111@users.noreply.github.com>
2025-02-17 21:27:26 +0800

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("hobbiesButton")
+    .addEventListener("click", function () {
+      alert("My hobbies include reading, social media, and playing badminton.");
+    });
+});
